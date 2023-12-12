9f83c69d9eca0ce51dcd04708d48bba479ad8037
Gavin Kimball <you@example.com>
2023-12-12 13:46:53 -0500

    add arrow for user convenience

--- src/components/BackGroundImage1.js
+++ src/components/BackGroundImage1.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Parallax } from "react-parallax";
 import image1 from "../images/IMG_5335.jpg";
 import linkedin from "../images/linkedin-logo.png";
 import github from "../images/GitHub-Logo-removebg-preview.png";
 import email from "../images/email-logo-removebg-preview.png";
-// import phone from "../images/phone_logo-removebg-preview.png";
 
 export const BackGroundImage1 = () => {
+	const [arrowClass, setArrowClass] = useState("");
+	const [arrowOpacity, setArrowOpacity] = useState("1");
+
+	const handleScroll = () => {
+		const scrollPos = window.scrollY;
+
+		if (scrollPos > 100) {
+			setArrowClass("x-large");
+			setArrowOpacity("0.5");
+		} else {
+			setArrowClass("");
+			setArrowOpacity("1");
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	const bgImageStyle = {
 		width: "100%",
 		height: "auto",
@@ -21,39 +43,29 @@ export const BackGroundImage1 = () => {
 			bgImageStyle={{ ...bgImageStyle }}
 		>
 			<div className="social-links">
-				<p>
+				<div className="each-link">
 					<a href="mailto:gavin.a.kimball@gmail.com">
 						<img src={email} alt="Email" />
 					</a>
-				</p>
-				<p>
-					<a
-						href="https://linkedin.com/in/gavinkimball"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+				</div>
+				<div className="each-link">
+					<a href="https://linkedin.com/in/gavinkimball">
 						<img src={linkedin} alt="LinkedIn" className="linkedin-logo" />
 					</a>
-				</p>
-				<p>
-					<a
-						href="https://github.com/Gkimbo"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+				</div>
+				<div className="each-link">
+					<a href="https://github.com/Gkimbo">
 						<img src={github} alt="GitHub" />
-					</a>{" "}
-				</p>
-				{/* <p>
-					<a href="tel:+17749913064">
-						<img src={phone} alt="Phone Number" />
 					</a>
-				</p> */}
+				</div>
 			</div>
 			<div className="content">
 				<h3>Hey, I'm</h3>
 				<h1>Gavin Kimball</h1>
 				<p>Software Developer</p>
+			</div>
+			<div className={`arrow ${arrowClass}`} style={{ opacity: arrowOpacity }}>
+				<i className="fa-solid fa-angles-down"></i>
 			</div>
 		</Parallax>
 	);
